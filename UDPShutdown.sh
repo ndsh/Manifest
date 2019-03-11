191f3194784b2878fe7525cc589d5d56027f44f8
Willy <willy@thegreeneyl.com>
2019-03-11 10:48:26 +0100

    Create UDPShutdown.sh

---
+++ UDPShutdown.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+nc -ulp 6000 | while read line 
+do
+	match=$(echo $line | grep -c 'Manifest,On/Off')
+        if [ $match -eq 1 ]; then
+            shutdown now
+        else
+        	echo -n "$line " | nc -4u -w1  localhost 6100
+        fi
+done
